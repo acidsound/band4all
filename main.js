810d441125b2435364dead106809090f474fb460
JaehoLee <spectrick@gmail.com>
2021-02-22 01:06:26 +0900

    EVENT_TYPE 변경, willMessage 추가. prettier 적용.

--- main.js
+++ main.js
@@ -6,237 +6,251 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 this.keyMap = {
-  '90': 60,
-  '83': 61,
-  '88': 62,
-  '68': 63,
-  '67': 64,
-  '86': 65,
-  '71': 66,
-  '66': 67,
-  '72': 68,
-  '78': 69,
-  '74': 70,
-  '77': 71,
-  '188': 72,
-  '76': 73,
-  '190': 74,
-  '186': 75,
-  '191': 76,
-  '81': 72,
-  '50': 73,
-  '87': 74,
-  '51': 75,
-  '69': 76,
-  '82': 77,
-  '53': 78,
-  '84': 79,
-  '54': 80,
-  '89': 81,
-  '55': 82,
-  '85': 83,
-  '73': 84,
-  '57': 85,
-  '79': 86,
-  '48': 87,
-  '80': 88,
-  '219': 89,
-  '187': 90,
-  '221': 91,
-}
-document.addEventListener('DOMContentLoaded', function () {
-  let userId = localStorage.getItem('userId')
+  90: 60,
+  83: 61,
+  88: 62,
+  68: 63,
+  67: 64,
+  86: 65,
+  71: 66,
+  66: 67,
+  72: 68,
+  78: 69,
+  74: 70,
+  77: 71,
+  188: 72,
+  76: 73,
+  190: 74,
+  186: 75,
+  191: 76,
+  81: 72,
+  50: 73,
+  87: 74,
+  51: 75,
+  69: 76,
+  82: 77,
+  53: 78,
+  84: 79,
+  54: 80,
+  89: 81,
+  55: 82,
+  85: 83,
+  73: 84,
+  57: 85,
+  79: 86,
+  48: 87,
+  80: 88,
+  219: 89,
+  187: 90,
+  221: 91,
+};
+document.addEventListener("DOMContentLoaded", function () {
+  let userId = localStorage.getItem("userId");
   if (!userId?.length) {
-    userId = UUID.generate()
-    localStorage.setItem('userId', userId)
+    userId = UUID.generate();
+    localStorage.setItem("userId", userId);
   }
-  console.log('init', userId)
-  const context = new AudioContext()
+  console.log("init", userId);
+  const context = new AudioContext();
 
-  const midi2Freq = (midi) => Math.pow(2, (midi - 69) / 12) * 440
+  const midi2Freq = (midi) => Math.pow(2, (midi - 69) / 12) * 440;
   const playNote = function (vel, freq) {
-    const c = context.currentTime
+    const c = context.currentTime;
 
-    const osc1 = context.createOscillator()
-    osc1.type = 'triangle'
-    osc1.frequency.value = freq
-    osc1.start(0)
+    const osc1 = context.createOscillator();
+    osc1.type = "triangle";
+    osc1.frequency.value = freq;
+    osc1.start(0);
 
-    const osc2 = context.createOscillator()
-    osc2.start(0)
-    osc2.type = 'square'
-    osc2.frequency.value = 3
+    const osc2 = context.createOscillator();
+    osc2.start(0);
+    osc2.type = "square";
+    osc2.frequency.value = 3;
 
-    const gain = context.createGain()
+    const gain = context.createGain();
 
-    osc1.connect(gain)
-    osc2.connect(osc1.frequency)
+    osc1.connect(gain);
+    osc2.connect(osc1.frequency);
 
-    const comp = context.createDynamicsCompressor()
-    comp.threshold.value = -50
-    comp.knee.value = 40
-    comp.ratio.value = 12
-    comp.reduction.value = -20
-    comp.attack.value = 0
-    comp.release.value = 0.25
+    const comp = context.createDynamicsCompressor();
+    comp.threshold.value = -50;
+    comp.knee.value = 40;
+    comp.ratio.value = 12;
+    comp.reduction.value = -20;
+    comp.attack.value = 0;
+    comp.release.value = 0.25;
 
-    gain.connect(comp)
-    const delay = context.createDelay(1)
-    delay.delayTime.value = 0.4
+    gain.connect(comp);
+    const delay = context.createDelay(1);
+    delay.delayTime.value = 0.4;
     //    comp.connect delay
     //    gain.connect delay
 
-    const panner = context.createStereoPanner()
-    panner.pan.setValueAtTime(-1, c)
-    panner.pan.linearRampToValueAtTime(1, c + 0.5)
-    panner.pan.linearRampToValueAtTime(-1, c + 1)
+    const panner = context.createStereoPanner();
+    panner.pan.setValueAtTime(-1, c);
+    panner.pan.linearRampToValueAtTime(1, c + 0.5);
+    panner.pan.linearRampToValueAtTime(-1, c + 1);
     //     panner.pan.linearRampToValueAtTime 1, c + 0.75
     //     panner.pan.linearRampToValueAtTime -1, c + 1
 
-    delay.connect(panner)
-    panner.connect(context.destination)
-    gain.connect(context.destination)
+    delay.connect(panner);
+    panner.connect(context.destination);
+    gain.connect(context.destination);
 
-    gain.gain.setValueAtTime(0, c)
-    gain.gain.linearRampToValueAtTime(vel, c + 0.02)
-    gain.gain.linearRampToValueAtTime(0, c + 0.5)
-    osc1.stop(context.currentTime + 1)
-    osc2.stop(context.currentTime + 1)
-  }
+    gain.gain.setValueAtTime(0, c);
+    gain.gain.linearRampToValueAtTime(vel, c + 0.02);
+    gain.gain.linearRampToValueAtTime(0, c + 0.5);
+    osc1.stop(context.currentTime + 1);
+    osc2.stop(context.currentTime + 1);
+  };
 
   // network stuff
   const EVENT_TYPE = {
     JOIN: 1,
     LEAVE: 2,
-    SIMPLEPEER: 3,
-  }
-  let peers = {}
-  let room = new URLSearchParams(location.search).get('room') || 'lobby'
-
+    SIGNALING: 3,
+  };
+  let peers = {};
+  let room = new URLSearchParams(location.search).get("room") || "lobby";
   const sendMessage = (msg) => {
-    const message = new Paho.MQTT.Message(
-      JSON.stringify({
-        id: userId,
-        ...msg,
-      }),
-    )
-    message.destinationName = room
-    client.send(message)
-  }
+    const message = Object.assign(
+      new Paho.MQTT.Message(
+        JSON.stringify({
+          id: userId,
+          ...msg,
+        })
+      ),
+      {
+        destinationName: room,
+      }
+    );
+    client.send(message);
+  };
   const onConnect = function () {
-    client.subscribe(room)
-    sendMessage({ type: EVENT_TYPE.JOIN })
-  }
+    client.subscribe(room);
+    client.subscribe(`${room}/leave`);
+    sendMessage({ type: EVENT_TYPE.JOIN });
+  };
   const onConnectionLost = function (responseObject) {
     if (responseObject.errorCode !== 0) {
-      console.log('onConnectionLost:', responseObject.errorMessage)
+      console.log("onConnectionLost:", responseObject.errorMessage);
     }
-  }
+  };
   const createPeerFactory = ({ peerId, initiator }) => {
-    const peer = new SimplePeer({ initiator })
-    peer.on('signal', (data) => {
-      console.log("peer:onSignal", data);
-      sendMessage({ type: EVENT_TYPE.SIMPLEPEER, data, peerId })
-    })
-    peer.on('connect', () => {
-      console.log('peer:establish datachannel')
-    })
-    peer.on('data', (data) => {
-      console.log('peer:datachannel:', data)
-      const msg = data
-      if (msg[0] === '@') {
-        const [key, vel, senderId] = msg.slice(1).split('/')
+    const peer = new SimplePeer({ initiator });
+    peer.on("signal", (signal) => {
+      console.log("peer:onSignal", signal);
+      sendMessage({ type: EVENT_TYPE.SIGNALING, signal, id: peerId });
+    });
+    peer.on("connect", () => {
+      console.log("peer:establish datachannel");
+    });
+    peer.on("data", (data) => {
+      console.log("peer:datachannel:", data);
+      const msg = data;
+      if (msg[0] === "@") {
+        const [key, vel, senderId] = msg.slice(1).split("/");
         if (senderId !== userId) {
-          playKey(vel, key, false)
+          playKey(vel, key, false);
         }
       }
-    })
-    peer.on('close', () => {
-      console.log('peer:close peer. id:', peerId)
-    })
-    peer.on('error', (err) => {
-      console.log('peer:err:', err)
-    })
-    return (peers[peerId] = peer)
-  }
-  const onMessageArrived = function (message) {
-    let parsedMessage
+    });
+    peer.on("close", () => {
+      console.log("peer:close peer. id:", peerId);
+    });
+    peer.on("error", (err) => {
+      console.log("peer:err:", err);
+    });
+    return (peers[peerId] = peer);
+  };
+  const onMessageArrived = function (message, topic) {
+    let parsedMessage;
     try {
-      parsedMessage = JSON.parse(message.payloadString)
+      parsedMessage = JSON.parse(message.payloadString);
     } catch (e) {
-      return
+      return;
     }
     if (parsedMessage.id === userId) {
-      return
+      return;
     }
     console.log("onMessageArrived", parsedMessage);
-    ;({
+    ({
       [EVENT_TYPE.JOIN]: ({ id }) =>
         createPeerFactory({ peerId: id, initiator: true }),
-      [EVENT_TYPE.LEAVE]: ({ id }) => peers[id]?.destory() || delete peers[id],
-      [EVENT_TYPE.SIMPLEPEER]: ({ id, signal }) =>
+      [EVENT_TYPE.LEAVE]: ({ id }) => peers[id]?.destroy() || delete peers[id],
+      [EVENT_TYPE.SIGNALING]: ({ id, signal }) =>
         (
           peers[id] || createPeerFactory({ peerId: id, initiator: false })
         ).signal(signal),
-    }[parsedMessage.type](parsedMessage))
-  }
+    }[parsedMessage.type](parsedMessage));
+  };
 
   var client = new Paho.MQTT.Client(
-    'mqtt.sheepal.ga',
+    "mqtt.sheepal.ga",
     Number(443),
-    `band4all_${userId}`,
-  )
-  client.onConnectionLost = onConnectionLost
-  client.onMessageArrived = onMessageArrived
+    `band4all_${userId}`
+  );
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
 
-  client.connect({ onSuccess: onConnect, useSSL: true })
+  const willMessage = Object.assign(
+    new Paho.MQTT.Message(
+      JSON.stringify({ id: userId, type: EVENT_TYPE.LEAVE })
+    ),
+    {
+      destinationName: `${room}/leave`,
+      qos: 0,
+      retained: false,
+    }
+  );
+
+  client.connect({ onSuccess: onConnect, useSSL: true, willMessage });
 
   const broadCast = function ({ destination, vel, key }) {
-    const message = new Paho.MQTT.Message(`@${key}/${vel}/${userId}`)
     Object.values(peers).forEach((peer) => {
-      peer.send(message)
-    })
-  }
+      peer.send(`@${key}/${vel}/${userId}`);
+    });
+  };
 
   playKey = function (vel, key, isLocal = true) {
-    let elem = document.querySelector(`[data-note='${key}']`)
-    if (!elem) return
+    let elem = document.querySelector(`[data-note='${key}']`);
+    if (!elem) return;
     if (vel > 0) {
-      elem.classList.add('active')
-      playNote(vel, midi2Freq(+key))
+      elem.classList.add("active");
+      playNote(vel, midi2Freq(+key));
     } else {
-      elem.classList.remove('active')
+      elem.classList.remove("active");
     }
     if (isLocal) {
-      broadCast({ destination: room, vel, key })
+      broadCast({ destination: room, vel, key });
     }
-  }
+  };
 
   navigator.requestMIDIAccess &&
     navigator.requestMIDIAccess().then((ma) =>
       Array.from(ma.inputs).forEach(
         (input) =>
           (input[1].onmidimessage = function ({ data }) {
-            const [msg, key, val] = data
+            const [msg, key, val] = data;
             if (msg === 144) {
-              playKey(val / 127.0, key)
+              playKey(val / 127.0, key);
             }
-          }),
-      ),
-    )
-  for (let downEvent of ['keydown']) {
+          })
+      )
+    );
+  for (let downEvent of ["keydown"]) {
     //, 'mousedown', 'touchstart']
     window.addEventListener(downEvent, function (e) {
-      const key = keyMap[e.which] || e.target.getAttribute('data-note')
-      playKey(1, key)
-    })
+      const key = keyMap[e.which] || e.target.getAttribute("data-note");
+      playKey(1, key);
+    });
   }
-  ;['keyup'].map((
-    upEvent, //, 'mouseup', 'touchend']
+  ["keyup"].map((
+    upEvent //, 'mouseup', 'touchend']
   ) =>
     window.addEventListener(upEvent, function (e) {
-      const key = keyMap[e.which] || e.target.getAttribute('data-note')
-      playKey(0, key)
-    }),
-  )
-})
+      const key = keyMap[e.which] || e.target.getAttribute("data-note");
+      playKey(0, key);
+    })
+  );
+});
