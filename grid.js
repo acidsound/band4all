59a07747b98cf0d25c37a1d6f1135ae67646431c
JaehoLee <spectrick@gmail.com>
2021-03-07 19:17:24 +0900

    grid 추가

--- grid.js
+++ grid.js
@@ -1,9 +1,11 @@
 console.log = function () {
   const logElement = document.querySelector("#log");
-  logElement.textContent +=
+  logElement.textContent =
     Array.from(arguments)
       .map((o) => (typeof o === "object" && JSON.stringify(o, null, 2)) || o)
-      .join(" ") + "\n";
+      .join(" ") +
+    "\n" +
+    logElement.textContent;
 };
 
 $ = (selectors, events = undefined, callback = undefined) => {
@@ -20,7 +22,7 @@ $ = (selectors, events = undefined, callback = undefined) => {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  $("#debug", ["click", "touchstart"], evt => {
+  $("#debug", ["click", "touchstart"], (evt) => {
     if ($("#log").classList.contains("hidden")) {
       $("#log").classList.remove("hidden");
     } else {
@@ -30,25 +32,28 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   document
     .querySelector(".modal>.dialog>.btnOk")
-    .addEventListener("click", async evt => {
+    .addEventListener("click", async (evt) => {
       document.querySelector(".modal").classList.add("hidden");
       context.state === "suspended" && (await context.resume());
       console.log("audioContext state", context.state);
       evt.preventDefault();
-      for (let downEvent of ["keydown"]) {
-        //, 'mousedown', 'touchstart']
-        window.addEventListener(downEvent, function (e) {
-          const key = keyMap[e.which] || e.target.getAttribute("data-note");
-          key && playKey(1, key);
-        });
-      }
-      ["keyup"].map((
-        upEvent //, 'mouseup', 'touchend']
-      ) =>
-        window.addEventListener(upEvent, function (e) {
-          const key = keyMap[e.which] || e.target.getAttribute("data-note");
-          key && playKey(0, key);
-        })
-      );
+      document.querySelectorAll("#grid .anchor").forEach((pad) => {
+        console.log(pad.getAttribute("data-note"));
+        pad.addEventListener(
+          "touchstart",
+          (e) => {
+            const touches = e.changedTouches;
+            const key = pad.getAttribute("data-note");
+            console.log("Touches", key, touches.length);
+            playKey(1, key);
+            e.preventDefault();
+          },
+          false
+        );
+      });
+      window.addEventListener("touchend", function (e) {
+        const key = e.target.getAttribute("data-note");
+        key && playKey(0, key);
+      });
     });
 });
